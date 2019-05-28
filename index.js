8259217e0f4d6138a112b863cd2128d19b8d1e32
Gurpreet Singh <preetzira08@gmail.com>
2019-05-29 04:24:33 +0530

    path routing

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
